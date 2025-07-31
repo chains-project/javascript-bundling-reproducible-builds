c9f17930a9ecfbcee1a2c52d39bfdaa1e4466b41
evitell <vitell@kth.se>
2025-07-31 17:05:11 +0200

    update oci images

--- oci_image1.nix
+++ oci_image1.nix
@@ -5,31 +5,31 @@
 let
   node_image = pkgs.dockerTools.pullImage {
     imageName = "node";
-    # lts-jod
+    # lts-jod, node 22 and bookworm
     imageDigest = "sha256:37ff334612f77d8f999c10af8797727b731629c26f2e83caa6af390998bdc49c";
     sha256 = "sha256-Ke3IRxmw+WSFE3Kt0/i7ryW2zIMGIVanNZeZ2/oaft4=";
 
   };
 in
 let
-# https://nix.dev/tutorials/nixos/building-and-running-docker-images.html
-image = pkgs.dockerTools.buildImage {
-  name = "image1";
-  tag = "latest";
-  # fromImage = "docker.io/library/archlinux";
-  fromImage = node_image;
+  # https://nix.dev/tutorials/nixos/building-and-running-docker-images.html
+  image = pkgs.dockerTools.buildImage {
+    name = "rb-lts-jod";
+    tag = "latest";
+    # fromImage = "docker.io/library/archlinux";
+    fromImage = node_image;
 
-  # https://ryantm.github.io/nixpkgs/builders/images/dockertools/
-  copyToRoot = pkgs.buildEnv {
-    name = "x";
-    paths = with pkgs; [ bash nodejs coreutils  ];
-    pathsToLink = [ "/bin" ];
+    # https://ryantm.github.io/nixpkgs/builders/images/dockertools/
+    # copyToRoot = pkgs.buildEnv {
+    #   name = "x";
+    #   paths = with pkgs; [ bash nodejs coreutils  ];
+    #   pathsToLink = [ "/bin" ];
+    # };
+    # config = {
+    #   Cmd =["ls" "/"];
+    # };
+    # includeNixDB=true;
   };
-  # config = {
-  #   Cmd =["ls" "/"];
-  # };
-  # includeNixDB=true;
-};
 in
 image
 #node_image
--- oci_image2.nix
+++ oci_image2.nix
@@ -1,20 +1,35 @@
 { pkgs ? import ./pkgs.nix { }
 , # pkgsLinux ? import ./pkgs.nix {}
 }:
-# https://nix.dev/tutorials/nixos/building-and-running-docker-images.html
-pkgs.dockerTools.buildImage {
-  name = "image2";
-  tag = "latest";
-  # fromImage = "docker.io/library/archlinux";
+# https://ryantm.github.io/nixpkgs/builders/images/dockertools/
+let
+  node_image = pkgs.dockerTools.pullImage {
+    imageName = "node";
+    # 24.4.1-alpine
+    imageDigest = "sha256:820e86612c21d0636580206d802a726f2595366e1b867e564cbc652024151e8a";
+    sha256 = "sha256-UYQ174DFDTui8hmnKUQ+AF3VOEvWhv9Zvc+a7XlFILA=";
 
-  # https://ryantm.github.io/nixpkgs/builders/images/dockertools/
-  copyToRoot = pkgs.buildEnv {
-    name = "x";
-    paths = with pkgs; [ bash coreutils nodejs_20 ];
-    pathsToLink = [ "/bin" ];
   };
-  # config = {
-  #   Cmd =["ls" "/"];
-  # };
-  # includeNixDB=true;
-}
+in
+let
+  # https://nix.dev/tutorials/nixos/building-and-running-docker-images.html
+  image = pkgs.dockerTools.buildImage {
+    name = "rb-24.4.1-alpine";
+    tag = "latest";
+    # fromImage = "docker.io/library/archlinux";
+    fromImage = node_image;
+
+    # https://ryantm.github.io/nixpkgs/builders/images/dockertools/
+    #copyToRoot = pkgs.buildEnv {
+    #  name = "x2";
+    #paths = with pkgs; [ bash nodejs coreutils  ];
+    #pathsToLink = [ "/bin" ];
+    #};
+    # config = {
+    #   Cmd =["ls" "/"];
+    # };
+    # includeNixDB=true;
+  };
+in
+image
+#node_image
