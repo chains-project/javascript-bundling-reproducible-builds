9aaffe3ebc85c2225053c160e842c20727032525
evitell <vitell@kth.se>
2025-09-02 18:04:23 +0200

    Delete shell.nix

--- shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    (pkgs.python3.withPackages (pyPkgs: [ pyPkgs.requests pyPkgs.pandas ]))
-    pkgs.nodejs_22
-
-  ];
-}
