a70c91089d07a63226fd302545c95958450aee5a
evitell <vitelle16@gmail.com>
2025-12-15 17:09:01 +0100

    add separate scripts to test reproducibility

---
+++ test_gh_reproducibility.py
@@ -0,0 +1,7 @@
+from parse import *
+
+if __name__ == "__main__":
+    print(f"Testing reproducibility status of Github projects for which data has previously been fetched")
+    sbd = "github_projects"
+    test_diffoscope(diffoscope_subdir=sbd)
+    print(f"done: output is in {sbd}")
---
+++ test_npm_reproducibility.py
@@ -0,0 +1,15 @@
+from parse import *
+
+if __name__ == "__main__":
+
+    n = 1000
+    data = test_stats.get_n_detailed(n)
+    # print(data)
+    repos = [test_stats.filter_detailed_npm_package_data(x) for x in data]
+    for index, _ in enumerate(repos):
+        repos[index]["commit"] = None
+
+    print(f"Testing reproducibility status of NPM packages for which data has previously been fetched")
+    sbd = "gh_diffoscope"
+    test_diffoscope(repos=repos)
+    print(f"done: output is in {sbd}")
