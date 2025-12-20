adf242b2e9838dce36f36e25ef392eb964de0e34
evitell <vitelle16@gmail.com>
2025-12-20 15:27:35 +0100

    print_diff_results: improve

--- print_diff_results.py
+++ print_diff_results.py
@@ -1,30 +1,29 @@
 import os
-DIFF_DATADIR="data/gh_diffoscope"
-DIFF_DATADIR="data/github_projects"
-import json
 from lib import utils
+
+DIFF_DATADIR = "data/gh_diffoscope"
+DIFF_DATADIR = "data/github_projects"
 
 
 def read_data(maxcount=-1):
     DIFF_DATADIR
     fs = os.listdir(DIFF_DATADIR)
-    fs.sort(key=lambda x:int(x.split("_")[0]))
+    fs.sort(key=lambda x: int(x.split("_")[0]))
     n = len(fs)
     if maxcount > 0:
         n = maxcount
-    res = [None]*n
-    for i, fp in enumerate([ f"{DIFF_DATADIR}/{f}" for f in fs ]):
+    res = [None] * n
+    for i, fp in enumerate([f"{DIFF_DATADIR}/{f}" for f in fs]):
         if i >= n:
             break
         print(fp)
         d = utils.read_json(fp)
         if type(d) is dict:
             d["fp"] = fp
-        res[i]=d 
-       
+        res[i] = d
+
     return res
 
-    
 
 def filter_diff_results():
     pass
@@ -40,14 +39,16 @@ def main():
             if "details" in diff.keys():
                 details = diff["details"]
                 # print(d.keys())
-                print(d["fp"])
+                print("fp", d["fp"])
                 for detail in details:
                     # print(detail.keys())
                     s1 = detail["source1"]
                     s2 = detail["source2"]
-                    print( s2,s2)
-    
-    print(f"total number of completed builds:", scount)
+                    print("s1, s2:", s1, s2)
+                print()
+
+    print("total number of completed builds:", scount)
+
 
 if __name__ == "__main__":
     main()
